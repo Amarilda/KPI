f9f29fbd16c17d62de78b579b9c9a45999e81fd6
Amarilda <Amarilda@users.noreply.github.com>
2022-12-14 11:41:49 +0100

    Create feelings2.py

---
+++ thinking the feelings/feelings2.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import datetime
+import shutil
+import os
+
+def ThinkingTheFeelings():
+    df = pd.read_csv("/Users/Edite/Documents/GitHub/KPI/feelings2.csv")
+    if pd.to_datetime(df.date[len(df)-1]) != (datetime.datetime.now()).date():
+        answer = []
+        datums = (datetime.datetime.now()).date()
+        print(datums.strftime('%d/%m/%y'))
+        answer.append(datums)                   
+        for i in df.columns[1:]:
+            print(i)
+            i = input() or 0
+            answer.append(i)           
+        df.loc[len(df)] = answer
+        df.to_csv("/Users/Edite/Documents/GitHub/KPI/feelings2.csv", index = False) 
+        print('Ta da!!!')
+    else:
+        print('You got this')
+        pass
